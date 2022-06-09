29a89aae3cfbba2b1387584a1e4de8fff95aea46
Kazeai-chan <107156418+Kazeai-chan@users.noreply.github.com>
2022-06-08 20:28:09 -0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+from array import array
+import numpy as np
+matriz = np.array([[0,1,2],[3,4,5]])
+for f in range(2):
+    for c in range(3):
+        print(matriz[f][c])
+
+print("")
+print(matriz[1,1])
